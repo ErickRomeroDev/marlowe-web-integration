7265e9959cbd75f7d8c560bdced197ff22b0a581
taina <tainabugs13@gmail.com>
2024-05-12 16:18:06 -0300

    w

--- components/wallet-select/index.tsx
+++ components/wallet-select/index.tsx
@@ -13,7 +13,8 @@ import { Dialog, DialogContent } from "@/components/ui/dialog";
 export const WalletSelect = () => {
   const [openInfo, setOpenInfo] = useState(false);
   const [loading, setLoading] = useState(true);
-  const { walletExtensions, connectWallet, balance, isOpen, onClose } = useCardanoStore();
+  const { walletExtensions, connectWallet, balance, isOpen, onClose } =
+    useCardanoStore();
 
   useEffect(() => {
     if (walletExtensions !== undefined) {
@@ -22,7 +23,7 @@ export const WalletSelect = () => {
   }, [walletExtensions]);
 
   const handleSelectWallet = (wallet: SupportedWalletName) => async () => {
-    await connectWallet(wallet);    
+    await connectWallet(wallet);
   };
 
   const toggleInfo = () => setOpenInfo((prev) => !prev);
@@ -38,25 +39,31 @@ export const WalletSelect = () => {
   if (walletExtensions.length === 0) {
     return (
       <Dialog open={isOpen} onOpenChange={onClose}>
-        <DialogContent>
-          <div className="my-16 flex w-full flex-col justify-center rounded-lg p-8 shadow-container md:w-2/3 lg:w-1/2 xl:w-1/3 2xl:w-1/4">
-            <p className="pb-2 text-2xl font-bold lg:text-3xl">No wallet found</p>
+        <div className="my-16 flex w-full flex-col justify-center rounded-lg p-8 shadow-container md:w-2/3 lg:w-1/2 xl:w-1/3 2xl:w-1/4">
+          <DialogContent>
+            <p className="pb-2 text-2xl font-bold lg:text-3xl">
+              No wallet found
+            </p>
             <div className="flex flex-col gap-4">
-              <p className="text-base text-m-disabled lg:text-lg">Please install a wallet to deploy a contract</p>
+              <p className="text-base text-m-disabled lg:text-lg">
+                Please install a wallet to deploy a contract
+              </p>
               <WalletsSupported />
             </div>
-          </div>
-        </DialogContent>
+          </DialogContent>
+        </div>
       </Dialog>
     );
   }
 
   return (
     <Dialog open={isOpen} onOpenChange={onClose}>
-      <DialogContent>
-        <div className="flex w-full flex-col justify-center rounded-lg p-8 shadow-container md:w-2/3 lg:w-1/2 xl:w-1/3 2xl:w-1/4">
+      <div className="flex w-full flex-col justify-center rounded-lg p-8 shadow-container md:w-2/3 lg:w-1/2 xl:w-1/3 2xl:w-1/4">
+        <DialogContent>
           <div className="flex items-center gap-3">
-            <p className="pb-2 text-2xl font-bold lg:text-3xl">Choose a wallet</p>
+            <p className="pb-2 text-2xl font-bold lg:text-3xl">
+              Choose a wallet
+            </p>
             <div className="relative">
               <Image
                 src="/info.svg"
@@ -73,20 +80,43 @@ export const WalletSelect = () => {
               )}
             </div>
           </div>
-          <p className="text-base text-m-disabled lg:text-lg">Please select a wallet to deploy a contract {balance}</p>
+          <p className="text-base text-m-disabled lg:text-lg">
+            Please select a wallet to deploy a contract {balance}
+          </p>
 
           <div className="flex flex-col gap-2 py-8">
             {walletExtensions.sort().map((wallet) => {
               return (
-                <div key={wallet.name} className="flex items-center justify-between gap-2 rounded-lg border p-4">
+                <div
+                  key={wallet.name}
+                  className="flex items-center justify-between gap-2 rounded-lg border p-4"
+                >
                   <div className="flex w-1/4 items-center gap-2">
-                    <Image src={wallet.icon} alt={wallet.name} height={ICON_SIZES.L} width={ICON_SIZES.L} />
-                    <p className="text-base font-bold capitalize">{wallet.name}</p>
+                    <Image
+                      src={wallet.icon}
+                      alt={wallet.name}
+                      height={ICON_SIZES.L}
+                      width={ICON_SIZES.L}
+                    />
+                    <p className="text-base font-bold capitalize">
+                      {wallet.name}
+                    </p>
                   </div>
                   <div className="w-1/2 sm:w-2/5 md:w-2/5 xl:w-1/3 2xl:min-w-min 2xl:max-w-min">
-                    <TailorButton size={SIZE.SMALL} color={COLORS.BLUE} onClick={handleSelectWallet(wallet.name as SupportedWalletName)}>
+                    <TailorButton
+                      size={SIZE.SMALL}
+                      color={COLORS.BLUE}
+                      onClick={handleSelectWallet(
+                        wallet.name as SupportedWalletName
+                      )}
+                    >
                       <div className="flex items-center justify-center gap-2">
-                        <Image src="/cardano.svg" alt={"C"} height={ICON_SIZES.XS} width={ICON_SIZES.XS} />
+                        <Image
+                          src="/cardano.svg"
+                          alt={"C"}
+                          height={ICON_SIZES.XS}
+                          width={ICON_SIZES.XS}
+                        />
                         <p className="font-normal text-black">Connect</p>
                       </div>
                     </TailorButton>
@@ -95,8 +125,8 @@ export const WalletSelect = () => {
               );
             })}
           </div>
-        </div>
-      </DialogContent>
+        </DialogContent>
+      </div>
     </Dialog>
   );
 };
