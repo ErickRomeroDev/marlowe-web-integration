d6ee0fb11fcd858aa8069baffa5e4c564ee8248b
taina <tainabugs13@gmail.com>
2024-05-20 16:58:02 -0300

    Sidebar

--- app/(dashboard)/_components/sidebar.tsx
+++ app/(dashboard)/_components/sidebar.tsx
@@ -1,44 +1,65 @@
+"use client"
+
 import Link from "next/link";
 import { SidebarButton, SidebarButtonProps } from "./sidebar-button";
+import Image from "next/image";
+import { usePathname } from "next/navigation";
 
 export const Sidebar = () => {
+  const pathname = usePathname();
   const buttons: SidebarButtonProps[] = [
-    { label: "Deposit test", imageSrc: "/unplug.svg", href: "/deposit-test" },
+    { label: "Deposit test", imageSrc: ["/deposit-white.svg","/deposit.svg"], href: "/deposit-test", currentUrl: pathname },
 
-    { label: "Deposit test", imageSrc: "/unplug.svg", href: "/deposit-test" },
+    { label: "Buy me a coffee", imageSrc: ["/coffee-white.svg","/coffee.svg"], href: "/buy-coffee", currentUrl: pathname },
+
+    { label: "Fund my project", imageSrc: ["/fund-me-white.svg","/fund-me.svg"], href: "/fund-project", currentUrl: pathname },
   ];
 
   return (
-    <aside className="fixed z-[1] left-0 bg-white h-full w-[220px] rounded-[30px] flex p-3 flex-col gap-y-4 text-[#121216]">
-      <div>
-        {buttons.map((button) => (
-          <SidebarButton
-            key={button.label}
-            label={button.label}
-            imageSrc={button.imageSrc}
-            href={button.href}
-          />
-        ))}
-      </div>
-      {/* <div className="py-4">
-        <div className="space-y-1 flex flex-col">
-          <div className="font-bold">
-            Contracts available
+    <div className="fixed inset-0 flex items-center">
+      <aside className="fixed flex z-[1] left-3 h-[calc(100%-24px)] w-[218px] drop-shadow-lg bg-white  rounded-[30px] pl-5 pr-4 py-8 flex-col justify-between">
+        <Link href="/">
+          <div className="flex items-center justify-center">
+            <Image
+              src="/logo-placeholder.svg"
+              alt="Logo"
+              height={40}
+              width={40}
+            />
           </div>
-          <Link
-            href="/deposit-test"
-            className="cursor-pointer"
-          >
-            1. Deposit Test
-          </Link>
-          <Link href="/buy-coffee" className="cursor-pointer">
-            2. Buy me a coffee
-          </Link>
-          <Link href="/fund-project" className="cursor-pointer">
-            3. Fund my project
-          </Link>
+        </Link>
+        <ul className="flex flex-col space-y-3">
+          {buttons.map((button) => (
+            <SidebarButton
+              key={button.label}
+              label={button.label}
+              imageSrc={button.imageSrc}
+              href={button.href}
+              currentUrl={button.currentUrl}
+            />
+          ))}
+        </ul>
+        <div className=" flex justify-center items-center gap-x-3">
+          <Image 
+          src="/discord-logo-black.svg"
+          alt="Discord"
+          height={28}
+          width={28}
+          />
+          <Image 
+          src="/x-logo.svg"
+          alt="Discord"
+          height={22}
+          width={22}
+          />
+          <Image 
+          src="/github-mark.svg"
+          alt="Discord"
+          height={26}
+          width={26}
+          />
         </div>
-      </div> */}
-    </aside>
+      </aside>
+    </div>
   );
 };
