223dc195e8dba8f613b082d36556ba33f2cd1e47
taina <tainabugs13@gmail.com>
2024-05-22 18:32:24 -0300

    no wallet connected

--- app/(dashboard)/layout.tsx
+++ app/(dashboard)/layout.tsx
@@ -1,18 +1,26 @@
+import { getCookies } from "@/lib/get-cookies";
 import { Navbar } from "./_components/navbar";
 import { Sidebar } from "./_components/sidebar";
+import { NoWallet } from "@/components/no-wallet";
 
 interface DashboardLayoutProps {
   children: React.ReactNode;
 }
 
 const DashboardLayout = ({ children }: DashboardLayoutProps) => {
+  const {address} = getCookies("walletInfo")
   return (
+    
     <main className="h-full bg-[#F5F5F8]">
       <Sidebar />
       <div className="pl-[230px] h-full flex ">
         <div className="h-full flex-1">
           <Navbar />
-          {children}
+          {address ? <>{children}</> : 
+          <div className="h-[calc(100%-88px)]">
+            <NoWallet />
+          </div>
+          }
         </div>
       </div>
     </main>
